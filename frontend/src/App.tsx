2934316e083838f2d62576fbc763bbcb7b8732e8
Way-Maker7 <Denis_Sabou@gmx.de>
2022-07-30 23:37:30 +0200

    style Home

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import {BrowserRouter, Routes, Route} from "react-router-dom";
 import Home from "./pages/Home";
-import './App.scss';
 import Blog from "./pages/Blog";
 
 function App() {
