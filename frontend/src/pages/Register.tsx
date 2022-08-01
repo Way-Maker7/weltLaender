32d69c17cc723716ea03dffcdc8aec85ae90efcc
Way-Maker7 <Denis_Sabou@gmx.de>
2022-08-01 22:06:39 +0200

    register page added

---
+++ frontend/src/pages/Register.tsx
@@ -0,0 +1,24 @@
+import {FormEvent, useState} from "react";
+
+export default function Register(){
+
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+    const [passwordRepeat, setPassordRepeat] = useState("");
+
+const register = (ev: FormEvent) => {
+    ev.preventDefault();
+}
+
+    return(
+        <div>
+            <h1>Get an Account</h1>
+            <form onSubmit={register}>
+                <input type="text" value={username} onChange={ev => setUsername(ev.target.value)} placeholder= "Username"/>
+                <input type="password" value={password} onChange={ev => setPassword(ev.target.value)} placeholder="Password"/>
+                <input type="password" value={passwordRepeat} onChange={ev => setPassordRepeat(ev.target.value)} placeholder= "Repeat the password"/>
+                <input type="submit" value="Register"/>
+            </form>
+        </div>
+    )
+}
