d49828f254a46406895ab55bb5093db5714a836f
Way-Maker7 <Denis_Sabou@gmx.de>
2022-08-08 10:52:29 +0200

    search input

--- frontend/src/components/GalleryItems.tsx
+++ frontend/src/components/GalleryItems.tsx
@@ -10,11 +10,11 @@ export default function GalleryItems(props: GalleryItemsProps) {
 
     return (
         <li className="Items">
-            <img src={props.country.flags.png} alt="flage"/>
+            <img src={props.country.flags.png} alt="flag"/>
             <div className="ItemsInfos">
                <h2> {props.country.translations.deu.common} </h2>
                 <h4>{"Capital : " + props.country.capital}</h4>
-               <p>Pop. {props.country.population}<br/></p>
+               <p>Pop. {props.country.population.toLocaleString()}</p>
             </div>
         </li>
     )
